a4a147a2e64598af76bf74f10741d3f5fc5e450d
adamchin1 <adammitchellchin@gmail.com>
2024-10-23 16:48:34 +0200

    Update FileProcessor.cs

--- FileProcessor.cs
+++ FileProcessor.cs
@@ -2,17 +2,14 @@
 {
     public class FileProcessor
     {
-
         public List<string[]> ReadFile(string filePath)
         {
-
             string[] lines = File.ReadAllLines(filePath);
 
             List<string[]> array = new();
 
             for (int i = 0; i < lines.Length; i++)
             {
-
                 if (!String.IsNullOrEmpty(lines[i]))
                 {
                     string[] parts = lines[i].Split(' ');
